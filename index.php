21391684d58c5f3b4e9ae07c653d52159b66aabe
danoscolaterais <35070666+danoscolaterais@users.noreply.github.com>
2022-02-04 22:40:06 +0000

    Update index.php
    Order items by date downloaded.
    Only apear in the feed if audio file is present.
    If jpg not avaiable load webp.

--- index.php
+++ index.php
@@ -4,37 +4,49 @@ $baseUrl = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 
 $output  = '<rss xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:wfw="http://wellformedweb.org/CommentAPI/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:sy="http://purl.org/rss/1.0/modules/syndication/" xmlns:slash="http://purl.org/rss/1.0/modules/slash/" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" xmlns:rawvoice="http://www.rawvoice.com/rawvoiceRssModule/" version="2.0">';
 $output .= '<channel>';
-$output .= '<title>Podcast dump</title>';
-$output .= '<description>Podcast feed from occasionally downloaded files.</description>';
+$output .= '<title>YT-DLP Podcast</title>';
+$output .= '<description>YT-DLP Podcast.</description>';
 $output .= '<link>' . $baseUrl . '</link>';
 $output .= '<image>';
 $output .= 		'<url>' . $baseUrl . 'podcasts.jpg</url>';
-$output .= 		'<title>Podcast dump</title>';
+$output .= 		'<title>YT-DLP Podcast</title>';
 $output .= 		'<link>' . $baseUrl . '</link>';
 $output .= '</image>';
 $output .= '<itunes:image href="' . $baseUrl . 'podcasts.jpg"/>';
 
 $files = glob("./data/*.info.json");
+usort($files, function($a, $b) {
+    return filemtime($b) - filemtime($a);
+});
 foreach($files as $file){
 
 $filecontents = file_get_contents($file);
 $json_a = json_decode($filecontents, true);
 
-$link    = $baseUrl . 'data/' . $json_a['id'] . ".mp3";
-$image    = $baseUrl . 'data/' . $json_a['id'] . ".jpg";
+if (file_exists($baseUrl . 'data/' . $json_a['id'] . ".m4a")) {
+	$link    = $baseUrl . 'data/' . $json_a['id'] . ".m4a";
+	#$image    = $baseUrl . 'data/' . $json_a['id'] . ".jpg";
+	if (file_exists($baseUrl . 'data/' . $json_a['id'] . ".jpg")) {
+    	$image = $baseUrl . 'data/' . $json_a['id'] . ".jpg";
+	} else {
+    	$image = $baseUrl . 'data/' . $json_a['id'] . ".webp";
+	}
 
-$output .= '<item>';
-$output .= '<title>' . htmlspecialchars($json_a['title']) . '</title>';
-$output .= '<description>' . htmlspecialchars($json_a['description']) . '</description>';
-$output .= '<link>' . $link . '</link>';
-$output .= '<enclosure url="' . $link . '" length="' . $json_a['filesize'] . '" type="audio/mpeg"/>';
-if (!is_null($json_a['upload_date'])) {
-	$output .= '<pubDate>' . DateTime::createFromFormat('Ymd', $json_a['upload_date'])->format('r') . '</pubDate>';
-}
-$output .= '<itunes:duration>' . $json_a['duration'] . '</itunes:duration>';
-$output .= '<itunes:image href="' . $image . '" />';
-$output .= '</item>';
-}
+	$output .= '<item>';
+	$output .= '<title>' . htmlspecialchars($json_a['channel'] . ' - ' . $json_a['title']) . '</title>';
+	$output .= '<description>' . htmlspecialchars($json_a['description']) . '</description>';
+	$output .= '<link>' . $link . '</link>';
+	$output .= '<enclosure url="' . $link . '" length="' . $json_a['filesize'] . '" type="audio/mpeg"/>';
+	if (!is_null($json_a['upload_date'])) {
+		$output .= '<pubDate>' . DateTime::createFromFormat('Ymd', $json_a['upload_date'])->format('r') . '</pubDate>';
+	}
+	$output .= '<itunes:duration>' . $json_a['duration'] . '</itunes:duration>';
+	$output .= '<itunes:image href="' . $image . '" />';
+	$output .= '</item>';
+	}
+ else {
+	continue;
+}}
 
 $output .= '</channel>';
 $output .= '</rss>';
